32e32513f8b4798198829b7f470e6877e0535366
William Silversmith <william.silversmith@gmail.com>
2020-11-11 00:49:04 -0500

    fix(cli): ensure license file can be found for "cloudfiles license" command

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import sys
 setuptools.setup(
   setup_requires=['pbr'],
   python_requires="~=3.6", # >= 3.6 < 4.0
+  include_package_data=True,
   extras_require={
     "deflate": [ "deflate" ],
   },
