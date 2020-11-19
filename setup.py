fa28cd0a08f57a950d1425681ca3a165863c455a
William Silversmith <william.silversmith@gmail.com>
2020-11-19 16:31:14 -0500

    chore: upgrade deflate to a standard install (#34)
    As of 0.2.0, three issues with deflate were fixed:
    - Memory leak
    - -fPIC for src instsall
    - Binaries released for major platforms

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setuptools.setup(
   setup_requires=['pbr'],
   python_requires="~=3.6", # >= 3.6 < 4.0
   include_package_data=True,
-  extras_require={
-    "deflate": [ "deflate" ],
-  },
   entry_points={
     "console_scripts": [
       "cloudfiles=cloudfiles_cli:main"
