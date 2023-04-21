7efdf0c9db3e228aa0f07d64579c83d9922bf2c1
William Silversmith <william.silversmith@gmail.com>
2023-04-21 03:09:34 -0400

    redesign: remove gevent monkey patch from CLI
    This causes big problems with boto's advanced functions
    since it uses threads under the hood.

--- automated_test.py
+++ automated_test.py
@@ -1,6 +1,3 @@
-import gevent.monkey
-gevent.monkey.patch_all(thread=False)
-
 import os
 import pytest
 import re
--- cloudfiles_cli/__init__.py
+++ cloudfiles_cli/__init__.py
@@ -1,4 +1,1 @@
-import gevent.monkey
-gevent.monkey.patch_all(thread=False)
-
 from .cloudfiles_cli import *
