976fe89f428753f22a2352eefb981154f028d60f
William Silversmith <william.silversmith@gmail.com>
2025-06-23 16:20:06 -0400

    fix: handle file uploads already ending in .gz

--- cloudfiles/interfaces.py
+++ cloudfiles/interfaces.py
@@ -120,6 +120,10 @@ EXT_TEST_SEQUENCE = [
   ('.bz2', 'bzip2')
 ]
 EXT_TEST_SEQUENCE_LOCK = threading.Lock()
+SUPPORTED_EXT = {
+  x[0]: x[1] 
+  for x in EXT_TEST_SEQUENCE if x[0] != '' 
+}
 
 def read_file(path, encoding, start, end):
   with open(path, 'rb') as f:
@@ -166,7 +170,12 @@ class FileInterface(StorageInterface):
   @classmethod
   def get_encoded_file_path(kls, path):
     global EXT_TEST_SEQUENCE
-    
+        
+    _, ext = os.path.splitext(path)
+    encoding = SUPPORTED_EXT.get(ext, None)
+    if encoding is not None:
+      return path, encoding
+
     with EXT_TEST_SEQUENCE_LOCK:
       seq = list(EXT_TEST_SEQUENCE)
 
